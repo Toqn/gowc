17e88e5595bfc0b3d526ef694b6d692654100ee3
thomas.kuhn <thomas.kuhn@zoho.eu>
2024-08-04 15:54:05 +0100

    Add word count

--- gowc.go
+++ gowc.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -12,6 +14,8 @@ func main() {
 	flag.StringVar(&fileName, "f", "", "File to check")
 
 	byteCountFlag := flag.Bool("c", false, "File byte count")
+	lineCountFlag := flag.Bool("l", false, "File line count")
+	wordCountFlag := flag.Bool("w", false, "File word count")
 
 	flag.Parse()
 
@@ -19,6 +23,13 @@ func main() {
 		fmt.Println("No file specified.")
 		os.Exit(1)
 	}
+
+	if !*byteCountFlag && !*lineCountFlag && !*wordCountFlag {
+		fmt.Println("No flags specified")
+		flag.Usage()
+		os.Exit(0)
+	}
+
 	absPath, err := filepath.Abs(fileName)
 	if err != nil {
 		fmt.Printf("Error getting absolut file path for file %s. Error: %s", fileName, err)
@@ -28,20 +39,46 @@ func main() {
 	if err != nil {
 		fmt.Printf("File %s does exist: %s", fileName, err)
 	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Printf("Error getting file info: %s", err)
+	}
 
 	if *byteCountFlag {
-		fileInfo, err := file.Stat()
-		if err != nil {
-			fmt.Printf("Error getting file info: %s", err)
-		}
-
 		byteCount := fileInfo.Size()
 		fmt.Printf("Bytes: %d\n", byteCount)
 	}
 
-	if !*byteCountFlag {
-		fmt.Println("No flags specified")
-		flag.Usage()
-		os.Exit(0)
+	if *lineCountFlag {
+		scanner := bufio.NewScanner(file)
+		lineCount := 0
+		for scanner.Scan() {
+			lineCount++
+		}
+
+		fmt.Printf("Number of lines: %d\n", lineCount)
 	}
+
+	if *wordCountFlag {
+		_, err := file.Seek(0, 0)
+		if err != nil {
+			fmt.Printf("Error seeking start of file: %s\n", err)
+		}
+		scanner := bufio.NewScanner(file)
+		wordCount := 0
+
+		for scanner.Scan() {
+			words := strings.Fields(scanner.Text())
+			wordCount += len(words)
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Printf("Encountered error while reading file: %s", err)
+		}
+
+		fmt.Printf("Number of words: %d\n", wordCount)
+	}
+
 }
